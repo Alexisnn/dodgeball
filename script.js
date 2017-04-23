14c0a3a20a789d99b6a13f3df51ba9ff53bb5f90
Alexis Nalls <alexis.n.nalls@gmail.com>
2017-04-23 20:18:59 +0000

    player

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     
-    
+   // $("body").keydown(function(event) {
+   // if (event.which === 39) {
+   //     $("img").css("left", $("img").offset().left + 40);
+   // }
     
     
     
