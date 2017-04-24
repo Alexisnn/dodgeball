6607483276a9eb802623651e89f175cc8d6bbddd
Alexis Nalls <alexis.n.nalls@gmail.com>
2017-04-24 12:51:14 +0000

    relative

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
    // if (event.which === 39) {
    //     $("img").css("left", $("img").offset().left + 40);
    // }
-    
+   // $("body").keydown(function(event) {
+    //moves right
+//    if (event.which === 39) {
+  //      $(".move").css("left", $(".move").offset().left + 40);
     
     
     
