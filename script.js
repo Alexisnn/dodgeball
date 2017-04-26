d9e4bf35056bf3847b9bd1503ed4b5a4d1d95be0
Alexis Nalls <alexis.n.nalls@gmail.com>
2017-04-26 03:05:00 +0000

    box

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
    // if (event.which === 39) {
    //     $("img").css("left", $("img").offset().left + 40);
    // }
-   // $("body").keydown(function(event) {
+    $("#border").keydown(function(event) {
     //moves right
-//    if (event.which === 39) {
-  //      $(".move").css("left", $(".move").offset().left + 40);
+    if (event.which === 39) {
+        $("#move").css("left", $("#move").offset().left + 40);
     
     
     
